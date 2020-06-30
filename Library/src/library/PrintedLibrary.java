f0621043c5639b8a7e5eff6bf0b0a74c5ae16a04
Martin <Martin@192.168.0.150>
2020-06-30 13:36:47 +0200

    implementacia rozhrania Serializable pre moznost ukladania stavu objektu

--- Library/src/library/PrintedLibrary.java
+++ Library/src/library/PrintedLibrary.java
@@ -6,9 +6,16 @@ public class PrintedLibrary extends Library<PrintedBook>{
 
 	private static PrintedLibrary library;
 	
+	private PrintedLibrary() {
+		loadLibrary();
+	}
+	
 	public static PrintedLibrary getInstance() {
 		return (library == null) ? new PrintedLibrary() : library;
 	}
 	
-	
+	// deserialization
+	private void loadLibrary() {
+		
+	}
 }
